697e19b6dac5c02bcb8e347529f102db2fb745a1
kutim <1252900197@qq.com>
2017-10-19 16:06:14 +0800

    使用　python 官方镜像
    添加构建脚本

--- test/build.sh
+++ test/build.sh
@@ -6,11 +6,13 @@ set -x
 cd ..
 HOME=`pwd`
 
-cd $HOME/docker-base/
+#cd $HOME/docker-base/
+#docker build --no-cache -t ubuntu:base .
 
-docker build --no-cache -t ubuntu:base .
+
+#cd $HOME/docker-iptables/
+#docker build --no-cache -t ubuntu:iptables .
 
 
-cd $HOME/docker-iptables/
-
-docker build --no-cache -t ubuntu:iptables .
+cd $HOME/pythonbase/
+docker build --no-cache -t python:base .
